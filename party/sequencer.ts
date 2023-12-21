19c37648339f3540cabcfb584822415cdf0451f6
Sunil Pai <threepointone@gmail.com>
2023-12-21 16:43:55 +0000

    add persistence

--- party/sequencer.ts
+++ party/sequencer.ts
@@ -1,29 +1,30 @@
 import type * as Party from "partykit/server";
-import { onConnect, unstable_getYDoc, type YPartyKitOptions } from "y-partykit";
-import type { Doc } from "yjs";
+import { onConnect, type YPartyKitOptions } from "y-partykit";
+// import type { Doc } from "yjs";
 import type { SerializedRoom } from "./sequencer-shared";
 
 export default class SequencerServer implements Party.Server {
   yjsOptions: YPartyKitOptions = {};
+  // eslint-disable-next-line no-useless-constructor
   constructor(public party: Party.Party) {}
 
-  getOpts() {
-    // options must match when calling unstable_getYDoc and onConnect
-    const opts: YPartyKitOptions = {
-      callback: { handler: (doc) => this.handleYDocChange(doc) },
-    };
-    return opts;
-  }
+  // getOpts() {
+  //   // options must match when calling unstable_getYDoc and onConnect
+  //   const opts: YPartyKitOptions = {
+  //     callback: { handler: (doc) => this.handleYDocChange(doc) },
+  //   };
+  //   return opts;
+  // }
 
   onConnect(conn: Party.Connection) {
-    return onConnect(conn, this.party, this.getOpts());
+    return onConnect(conn, this.party, { persist: true });
   }
 
-  handleYDocChange(doc: Doc) {
-    //console.log("ydoc changed");
-    // called on every ydoc change
-    // no-op
-  }
+  // handleYDocChange(doc: Doc) {
+  //   //console.log("ydoc changed");
+  //   // called on every ydoc change
+  //   // no-op
+  // }
 
   async onRequest(req: Party.Request) {
     // /checkpoint is used to store a saved room state
